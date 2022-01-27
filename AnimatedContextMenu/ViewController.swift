2d71e9603e404fb11cd1ec011ccedf29d535c03b
Rudenko Elizabeth <rudenkoelizabeth@gmail.com>
2022-01-27 09:36:10 +0300

    Add init project

---
+++ AnimatedContextMenu/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AnimatedContextMenu
+//
+//  Created by Elizaveta Rudenko on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
