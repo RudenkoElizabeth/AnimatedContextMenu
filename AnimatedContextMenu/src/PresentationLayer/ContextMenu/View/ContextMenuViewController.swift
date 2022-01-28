efa76e1e33ea4b2c2cd579989abe12f250d7dfc9
Rudenko Elizabeth <rudenkoelizabeth@gmail.com>
2022-01-28 17:16:41 +0300

    Add action on background tap + update setup animation

--- AnimatedContextMenu/src/PresentationLayer/ContextMenu/View/ContextMenuViewController.swift
+++ AnimatedContextMenu/src/PresentationLayer/ContextMenu/View/ContextMenuViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 class ContextMenuViewController: UIViewController, ContextMenuViewInput {
     
     @IBOutlet private weak var contextMenuView: UIView!
-    @IBOutlet private weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var topHeight: NSLayoutConstraint!
+    @IBOutlet private weak var contextMenuHeight: NSLayoutConstraint!
+    @IBOutlet private weak var contextMenuBottom: NSLayoutConstraint!
     
     var output: ContextMenuViewOutput!
-    private let popupOffset: CGFloat = 460
+    private var conttextMenuOffset: CGFloat = 0
     private var currentState: ContextMenuConstants.State = .closed
     private var runningAnimator: UIViewPropertyAnimator?
     private var animationProgress: CGFloat = 0
     private lazy var panRecognizer: UIPanGestureRecognizer = {
         let recognizer = UIPanGestureRecognizer()
         recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+    private lazy var tapRecognizer: UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(tabBackgroundAction))
         return recognizer
     }()
     
@@ -29,35 +36,35 @@ class ContextMenuViewController: UIViewController, ContextMenuViewInput {
     }
     
     func setupInitialState() {
+        conttextMenuOffset = contextMenuHeight.constant - topHeight.constant
         contextMenuView.addGestureRecognizer(panRecognizer)
+        view.addGestureRecognizer(tapRecognizer)
     }
     
     func showContextMenu() {
         animateTransitionIfNeeded(to: .open)
     }
     
-    /// Animates the transition, if the animation is not already running.
+    @objc private func tabBackgroundAction() {
+        animateTransitionIfNeeded(to: .closed)
+    }
+    
+    // Animates the transition, if the animation is not already running.
     private func animateTransitionIfNeeded(to state: ContextMenuConstants.State) {
-        
-        // ensure that the animators array is empty (which implies new animations need to be created)
         guard runningAnimator == nil else { return }
-        
-        // an animator for the transition
         let transitionAnimator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: {
             switch state {
             case .open:
-                self.bottomConstraint.constant = 0
+                self.contextMenuBottom.constant = 0
                 self.contextMenuView.layer.cornerRadius = 20
                 self.view.backgroundColor = .black.withAlphaComponent(0.5)
             case .closed:
-                self.bottomConstraint.constant = self.popupOffset
+                self.contextMenuBottom.constant = self.conttextMenuOffset
                 self.contextMenuView.layer.cornerRadius = 0
                 self.view.backgroundColor = .clear
             }
             self.view.layoutIfNeeded()
         })
-        
-        // the transition completion block
         transitionAnimator.addCompletion { [self] position in
             runningAnimator = nil
             switch position {
@@ -68,16 +75,14 @@ class ContextMenuViewController: UIViewController, ContextMenuViewInput {
             default:
                 return
             }
-            // manually reset the constraint positions
             switch self.currentState {
             case .open:
-                self.bottomConstraint.constant = 0
+                self.contextMenuBottom.constant = 0
             case .closed:
-                self.bottomConstraint.constant = self.popupOffset
+                self.contextMenuBottom.constant = self.conttextMenuOffset
                 self.dismiss(animated: false)
             }
         }
-        
         transitionAnimator.startAnimation()
         runningAnimator = transitionAnimator
     }
@@ -89,38 +94,31 @@ class ContextMenuViewController: UIViewController, ContextMenuViewInput {
             runningAnimator?.pauseAnimation()
             animationProgress = runningAnimator?.fractionComplete ?? 0
         case .changed:
-            
-            // variable setup
             let translation = recognizer.translation(in: contextMenuView)
-            var fraction = -translation.y / popupOffset
-            
-            // adjust the fraction for the current state and reversed state
-            if currentState == .open { fraction *= -1 }
-            if runningAnimator?.isReversed == true { fraction *= -1 }
+            var fraction = -translation.y / conttextMenuOffset
+            let isAdjustFraction = currentState == .open || runningAnimator?.isReversed == true
+            if isAdjustFraction { fraction *= -1 }
             runningAnimator?.fractionComplete = fraction + animationProgress
         case .ended:
             let yVelocity = recognizer.velocity(in: contextMenuView).y
-            let shouldClose = yVelocity > 0
-            
-            // if there is no motion, continue all animations and exit early
-            if yVelocity == 0 {
+            guard yVelocity != 0 else {
                 runningAnimator?.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-                break
+                return
             }
-            
+            let shouldClose = yVelocity > 0
             if let isReversed = runningAnimator?.isReversed {
                 switch currentState {
                 case .open:
-                    if !shouldClose && !isReversed { runningAnimator?.isReversed = !isReversed }
-                    if shouldClose && isReversed { runningAnimator?.isReversed = !isReversed }
+                    if !shouldClose && !isReversed { runningAnimator?.isReversed.toggle() }
+                    if shouldClose && isReversed { runningAnimator?.isReversed.toggle() }
                 case .closed:
-                    if shouldClose && !isReversed { runningAnimator?.isReversed = !isReversed }
-                    if !shouldClose && isReversed { runningAnimator?.isReversed = !isReversed }
+                    if shouldClose && !isReversed { runningAnimator?.isReversed.toggle() }
+                    if !shouldClose && isReversed { runningAnimator?.isReversed.toggle() }
                 }
             }
             runningAnimator?.continueAnimation(withTimingParameters: nil, durationFactor: 0)
         default:
-            ()
+            return
         }
     }
 }
